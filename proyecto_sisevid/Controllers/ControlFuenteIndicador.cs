0458dd41c40ab93abc32d57e560b10aaa2aa8196
Alexisv07 <94263714+Alexisv07@users.noreply.github.com>
2023-04-10 21:29:03 -0500

    FuenteIndicador

---
+++ proyecto_sisevid/Controllers/ControlFuenteIndicador.cs
@@ -0,0 +1,86 @@
+﻿using proyecto_sisevid.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_sisevid.Controllers
+{
+    public class ControlFuenteIndicador
+    {
+        FuenteIndicador objFuenteIndicador;
+        string baseDeDatos;
+
+        public ControlFuenteIndicador(FuenteIndicador objFuenteIndicador)
+        {
+            this.objFuenteIndicador = objFuenteIndicador;
+            baseDeDatos = "bd_sisevid_015224.mdf";
+        }
+
+        public ControlFuenteIndicador()
+        {
+            this.objFuenteIndicador = null;
+            baseDeDatos = "bd_sisevid_015224.mdf";
+        }
+
+        public void guardar()
+        {
+            int fkidfuente = objFuenteIndicador.Fkidfuente;
+            int fkidindicador = objFuenteIndicador.Fkidindicador;
+
+            string comandoSQL =
+            String.Format("INSERT INTO fuente_indicador VALUES ('{0}',{1})", fkidfuente, fkidindicador);
+            ControlConexion objControlConexion = new ControlConexion(baseDeDatos);
+            objControlConexion.abrirBD();
+            objControlConexion.ejecutarComandoSQL(comandoSQL);
+            objControlConexion.cerrarBD();
+        }
+
+        public string[,] consultarFuente_por_Indicador()
+        {
+            string msg = "ok";
+            int i;
+            string[,] matFuenteIndicador = null;
+            int Fkidfuente = objFuenteIndicador.Fkidfuente;
+            string comandoSQL =
+            String.Format("SELECT fuente.id,indicador.id " +
+                        "FROM fuente INNER JOIN indicador ON fuente.id=indicador.fkidindicador" +
+                        " WHERE fuente.fkidfuente='{0}'", Fkidfuente);
+            ControlConexion objControlConexion = new ControlConexion(baseDeDatos);
+            objControlConexion.abrirBD();
+            DataSet objDataSet = objControlConexion.ejecutarConsultasSql(comandoSQL);
+            try
+            {
+                if (objDataSet.Tables[0].Rows.Count > 0)
+                {
+                    i = 0;
+                    matFuenteIndicador = new string[objDataSet.Tables[0].Rows.Count, 2];
+                    while (i < objDataSet.Tables[0].Rows.Count)
+                    {
+                        matFuenteIndicador[i, 0] = objDataSet.Tables[0].Rows[i][0].ToString();
+                        matFuenteIndicador[i, 1] = objDataSet.Tables[0].Rows[i][1].ToString();
+                        i++;
+                    }
+                    objControlConexion.cerrarBD();
+                }
+            }
+            catch(Exception objExcetion)
+            {
+                msg = objExcetion.Message;
+            }
+            return matFuenteIndicador;
+        }
+        public void borrarDelaFuente()
+        {
+            int Fkidfuente = objFuenteIndicador.Fkidfuente;
+
+            string comandoSQL =
+            String.Format("DELETE FROM fuente_indicador WHERE Fkidfuente='{0}'", Fkidfuente);
+            ControlConexion objControlConexion = new ControlConexion(baseDeDatos);
+            objControlConexion.abrirBD();
+            objControlConexion.ejecutarComandoSQL(comandoSQL);
+            objControlConexion.cerrarBD();
+        }
+    }
+}
---
+++ proyecto_sisevid/Models/FuenteIndicador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_sisevid.Models
+{
+    public class FuenteIndicador
+    {
+        int fkidfuente;
+        int fkidindicador;
+
+        public FuenteIndicador(int fkidfuente, int fkidindicador)
+        {
+            this.fkidfuente = fkidfuente;
+            this.fkidindicador = fkidindicador;
+        }
+
+        public int Fkidfuente { get => fkidfuente; set => fkidfuente = value; }
+        public int Fkidindicador { get => fkidindicador; set => fkidindicador = value; }
+
+        public FuenteIndicador()
+        {
+            this.fkidfuente=0;
+            this.fkidindicador=0;
+        }
+    }
+}
